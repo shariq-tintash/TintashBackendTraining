8a85f714797050f97d274d0e1e6d1c0aba54aa89
shariq-tintash <shariq.rashid@tintash.com>
2022-07-15 10:38:36 +0500

    problem3

---
+++ week1/problem3.js
@@ -0,0 +1,27 @@
+function sum(...args) {
+
+    function add(...args2) {
+      return sum(...args, ...args2);
+    }
+  
+    const curr = [...args].reduce((acc, curr) => acc + curr, 0);
+    add.value = curr;
+    return add;
+  }
+
+
+
+console.log(sum(1).value);
+console.log(sum(1)(2).value);
+console.log(sum(1)(2)(3).value);
+console.log(sum(1)(2)(3)(4).value);
+console.log(sum(1)(2)(3)(4)(5).value);
+console.log(sum(1)(2)(3)(4)(5)(6).value);
+
+// Output
+// 1
+// 3
+// 6
+// 10
+// 15
+// 21
